5acdced2cfc5d4ee2a9aa7cee15a031c443620e4
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2024-06-17 23:48:05 +0530

    spaces

--- Practical 01/5.scala
+++ Practical 01/5.scala
@@ -1,18 +1,19 @@
 object RunningTimeCalculator {
+    
     def totalRunningTime(easyPaceKm: Double, tempoPaceKm: Double): Double = {
 
-      val easyPaceMinPerKm = 8.0 
-      val tempoPaceMinPerKm = 7.0 
+    val easyPaceMinPerKm = 8.0 
+    val tempoPaceMinPerKm = 7.0 
 
-      val easyDistance1 = 2.0
-      val tempoDistance = 3.0
-      val easyDistance2 = 2.0
+    val easyDistance1 = 2.0
+    val tempoDistance = 3.0
+    val easyDistance2 = 2.0
 
-      val easyTime1 = easyDistance1*easyPaceMinPerKm
-      val tempoTime = tempoDistance*tempoPaceMinPerKm
-      val easyTime2 = easyDistance2*easyPaceMinPerKm
+    val easyTime1 = easyDistance1*easyPaceMinPerKm
+    val tempoTime = tempoDistance*tempoPaceMinPerKm
+    val easyTime2 = easyDistance2*easyPaceMinPerKm
 
-        easyTime1 + tempoTime + easyTime2
+    easyTime1 + tempoTime + easyTime2
     }
 
     def main(args: Array[String]): Unit = {
